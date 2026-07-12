2558a7b4fb68f427229c871be25b04d7f14cd7a7
kyooni18 <kyoung01122189@gmail.com>
2026-07-13 02:11:13 +0900

    Update Homebrew formula to 0.1.2

--- Formula/swiftpico.rb
+++ Formula/swiftpico.rb
@@ -1,8 +1,8 @@
 class Swiftpico < Formula
   desc "Project and USB flashing tool for Swift Pico firmware"
   homepage "https://github.com/kyooni18/swiftpico"
-  url "https://github.com/kyooni18/swiftpico/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "e4a0ebc597c78d6ccdc3fce031119f8fd3abc2ddbed103763dd23a61f903a0b3"
+  url "https://github.com/kyooni18/swiftpico/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "71802d1ee645685de37c7e8c7727bcdac053da086464160b647bbd13e5c3f33f"
   depends_on "picotool"
   depends_on xcode: :build
 
