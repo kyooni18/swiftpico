1491f00ce6f28a257125b8bd26c6e462224d6223
kyooni18 <kyoung01122189@gmail.com>
2026-07-14 02:27:21 +0900

    Refresh formula checksum for nightly release tag

--- Formula/swiftpico.rb
+++ Formula/swiftpico.rb
@@ -2,7 +2,7 @@ class Swiftpico < Formula
   desc "Project and USB flashing tool for Swift Pico firmware"
   homepage "https://github.com/kyooni18/swiftpico"
   url "https://github.com/kyooni18/swiftpico/archive/refs/tags/v0.1.4.tar.gz"
-  sha256 "c2d42466801abd1a966a25a813e0b997e906839c1157c2c37a8fb0166be30c65"
+  sha256 "9ca7867236cecea543ecba3748c2cbc1ef238bad3a9615eb7b6e8de9f5b25f71"
   depends_on "picotool"
   depends_on xcode: :build
 
