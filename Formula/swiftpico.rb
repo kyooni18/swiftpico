1d24e023be15f2de2725a31acae668f20f6dbab4
kyooni18 <kyoung01122189@gmail.com>
2026-07-14 02:42:39 +0900

    Refresh formula checksum after entry point fix

--- Formula/swiftpico.rb
+++ Formula/swiftpico.rb
@@ -2,7 +2,7 @@ class Swiftpico < Formula
   desc "Project and USB flashing tool for Swift Pico firmware"
   homepage "https://github.com/kyooni18/swiftpico"
   url "https://github.com/kyooni18/swiftpico/archive/refs/tags/v0.1.4.tar.gz"
-  sha256 "3d000e14483274facee8bec84d5422d47f14c2a06fca043aec6f592d3d73f73a"
+  sha256 "65accf746564cadc93dcc33ecf402a5887f68d970eef18429acb0a65dc1438c2"
   depends_on "picotool"
   depends_on xcode: :build
 
