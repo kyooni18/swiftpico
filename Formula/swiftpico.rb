8e8376b127aeb3fdeae3da06301a5e24c161c69d
kyooni18 <kyoung01122189@gmail.com>
2026-07-15 22:08:30 +0900

    Update SwiftPico formula for v0.2.10

--- Formula/swiftpico.rb
+++ Formula/swiftpico.rb
@@ -2,7 +2,7 @@ class Swiftpico < Formula
   desc "Project and USB flashing tool for Swift Pico firmware"
   homepage "https://github.com/kyooni18/swiftpico"
   url "https://github.com/kyooni18/swiftpico/archive/refs/tags/v0.2.10.tar.gz"
-  sha256 "REPLACE_AFTER_TAG_PUSH"
+  sha256 "2a1f016bf0e59f4a6bd3db0175f2f22383d8031e2ce251c28a2cd48dca70e8c1"
   depends_on xcode: :build
   depends_on "picotool"
 
